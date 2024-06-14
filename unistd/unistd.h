1db52da51c22c7fae6c90f13c4cd0ef8e64977e4
Thomas <semoir.dense-0h@icloud.com>
2024-06-14 01:43:17 +0000

    Update Minilibx and make it build on window and linux (#1)

---
+++ unistd/unistd.h
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------
+ * unistd.h
+ *
+ * Author: Thomas Choquet <thomas.publique@icloud.com>
+ * Date: 2024/06/08 09:55:25
+ * ---------------------------------------------------
+ */
+
+#ifndef UNISTD_H
+#define UNISTD_H
+
+#include <stdlib.h>
+typedef long ssize_t;
+
+#endif // UNISTD_H
